d7351e40bf17a1946a757483060296768c83d07a
dave <dave@armchair.mb.ca>
2019-12-04 22:51:00 +0000

    Remove unneeded imports.

--- TestsRun/src/test/java/org/reldb/wrapd/tests/database/test/TestPostgreSQL.java
+++ TestsRun/src/test/java/org/reldb/wrapd/tests/database/test/TestPostgreSQL.java
@@ -1,16 +1,11 @@
 package org.reldb.wrapd.tests.database.test;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
-import org.reldb.wrapd.sqldb.Database;
 import org.reldb.wrapd.sqldb.Query;
-import org.reldb.wrapd.sqldb.ResultSetToTuple;
 import org.reldb.wrapd.tests.database.shared.DatabaseConfigurationAndSetup;
-import org.reldb.wrapd.tuples.Tuple;
 import org.reldb.wrapd.tuples.generated.TestSelect;
 
 /**
