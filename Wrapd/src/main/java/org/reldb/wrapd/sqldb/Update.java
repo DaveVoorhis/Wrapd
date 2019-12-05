9a1948d037440498c1ba20e3600cc0b10901b80f
davevoorhis <dave@armchair.mb.ca>
2019-12-05 15:12:20 +0000

    Work-in-progress on Update class.

--- Wrapd/src/main/java/org/reldb/wrapd/sqldb/Update.java
+++ Wrapd/src/main/java/org/reldb/wrapd/sqldb/Update.java
@@ -1,9 +1,17 @@
 package org.reldb.wrapd.sqldb;
 
+import java.sql.Connection;
+
 import org.reldb.wrapd.tuples.Tuple;
 
 public class Update {
-	
+
+	public static boolean insert(Database database, Connection connection, String tableName, Tuple tupleClass) {
+		String sql = "INSERT INTO " + tableName + " VALUES (";
+		
+		return true;
+	}
+		
 	public static boolean insert(Database database, String tableName, Tuple tupleClass) {
 		
 		return true;
