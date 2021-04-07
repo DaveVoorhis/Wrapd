789e956e10fb77cbfd69dfba03b093c18e2ee588
dave <dave@armchair.mb.ca>
2021-04-07 23:22:39 +0100

    Remove unused parameter definition.

--- src/test/resources/Queries/QueryDefinition02.java
+++ src/test/resources/Queries/QueryDefinition02.java
@@ -2,6 +2,6 @@ package org.reldb.wrapd.sqldb;
 
 public class QueryDefinition02 extends QueryDefinition {
     public QueryDefinition02() {
-        super("Query02","SELECT * FROM $$tester WHERE x > ? AND x < ?");
+        super("Query02","SELECT * FROM $$tester");
     }
 }
