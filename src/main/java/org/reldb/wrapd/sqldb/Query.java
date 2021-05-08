cd3ac6f9bc3374c018410ee0aa5e8545bc866fe9
dave <dave@armchair.mb.ca>
2021-05-08 01:11:29 +0100

    Remove completed TODOs.

--- src/main/java/org/reldb/wrapd/sqldb/Query.java
+++ src/main/java/org/reldb/wrapd/sqldb/Query.java
@@ -7,14 +7,12 @@ public class Query<T extends Tuple> {
     private final Class<T> tupleClass;
     private final Object[] arguments;
 
-    // TODO probably should be protected, since eventually only subclasses of this should be exposed
     protected Query(String queryText, Class<T> tupleClass, Object... arguments) {
         this.text = queryText;
         this.tupleClass = tupleClass;
         this.arguments = arguments;
     }
 
-    // TODO probably should be protected, since eventually only subclasses of this should be exposed
     protected Query(String queryText, Class<T> tupleClass) {
         this(queryText, tupleClass, new Object[] {});
     }
